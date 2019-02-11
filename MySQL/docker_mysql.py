7413980a6ea400fc7007477be046808972efb1a8
esukhanov <beardrinker@yandex.ru>
2019-02-11 17:07:11 +0300

    +

--- MySQL/docker_mysql.py
+++ MySQL/docker_mysql.py
@@ -4,9 +4,9 @@ import docker
 
 
 image_name='mysql'
-
+mysql_pass='123456'
 client = docker.from_env()
 image =client.images.pull(image_name+':latest')
 container=client.containers.create(image_name, detach=True)
-client.containers.run(image_name)
+client.containers.run(image_name,command='run --name some-mysql -e MYSQL_ROOT_PASSWORD='+mysql_pass+' -d '+ image_name+':latest')
 print (container.status)
