5ae0466ae40711e8949a22dde4a5e4aa2e1c3712
esukhanov <beardrinker@yandex.ru>
2019-10-20 12:09:57 +0300

    +

--- asyncserver/io_server.py
+++ asyncserver/io_server.py
@@ -23,4 +23,11 @@ async def server(reader,writer):
 
 loop = asyncio.get_event_loop()
 coro = asyncio.start_server(server, '127.0.0.1', 8888, loop=loop)
-server = loop.run_until_complete(coro)
+#server = loop.run_until_complete(coro)
+
+# Serve requests until Ctrl+C is pressed
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
